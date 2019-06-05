2beb89211475c7791430d8953f72c2e61740cc4c
tan <tan@gaing.cn>
2019-06-05 15:37:37 +0800

    添加随机数工具类

---
+++ src/Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"GenerateGuId: {Utils.Randoms.GenerateGuId("n")}");
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
